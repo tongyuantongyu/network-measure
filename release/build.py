9eb3d34041924547ef4104be3aedea5562a666f0
Yuan Tong <tongyuan200097@gmail.com>
2021-12-19 11:27:55 +0800

    add build script

---
+++ release/build.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+import glob
+import datetime
+
+ver = subprocess.run("git describe --tags", shell=True, capture_output=True)
+ver = ver.stdout.decode().strip()
+
+full_ver = subprocess.run("git describe --always --dirty --long --tags", shell=True, capture_output=True)
+full_ver = full_ver.stdout.decode().strip()
+
+cur = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+
+t = [['linux', 'amd64'],
+     ['linux', 'arm'],
+     ['linux', 'arm64'],
+     ['linux', 'mips'],
+     ['linux', 'mipsle'],
+     ['windows', 'amd64'],
+]
+
+for o, a in t:
+    subprocess.run(
+        f'''SET GOOS={o}&&SET GOARCH={a}&&'''
+        f'''go build -tags="jsoniter" -ldflags="-s -w '''
+        f'''-X main.fullVersion={full_ver} -X \'main.buildDate={cur}\'" '''
+        f'''-o network-measure-http_{o}_{a}{".exe" if o == "windows" else ""} '''
+        f'''../exec/http''', shell=True
+    )
+    subprocess.run(
+        f'''SET GOOS={o}&&SET GOARCH={a}&&'''
+        f'''go build -tags="jsoniter" -ldflags="-s -w '''
+        f'''-X main.version={ver} -X main.fullVersion={full_ver} -X \'main.buildDate={cur}\'" '''
+        f'''-o network-measure-ws_{o}_{a}{".exe" if o == "windows" else ""} '''
+        f'''../exec/ws''', shell=True
+    )
+
+subprocess.run(
+    f'''SET GOOS={o}&&SET GOARCH={a}&&'''
+    f'''go build -tags="jsoniter" -ldflags="-s -w -H windowsgui '''
+    f'''-X main.fullVersion={full_ver} -X \'main.buildDate={cur}\'" '''
+    f'''-o network-measure-http_windowsgui_amd64.exe '''
+    f'''../exec/http''', shell=True
+)
+subprocess.run(
+    f'''SET GOOS={o}&&SET GOARCH={a}&&'''
+    f'''go build -tags="jsoniter" -ldflags="-s -w -H windowsgui '''
+    f'''-X main.version={ver} -X main.fullVersion={full_ver} -X \'main.buildDate={cur}\'" '''
+    f'''-o network-measure-ws_windowsgui_amd64.exe '''
+    f'''../exec/ws''', shell=True
+)
